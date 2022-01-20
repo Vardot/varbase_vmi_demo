4914dd75d313ec7516b41075ffcf93e39c15a847
Rajab Natshah <rajabn@gmail.com>
2022-01-20 18:57:30 +0200

    Add Varbase Media Default Content module to help with testing and demoing new Varbase 9.1.x builds #20

---
+++ scripts/generate-varbase-testing-content.sh
@@ -0,0 +1,20 @@
+#!/bin/usr/env bash
+
+
+## cd PROJECT_DIR_NAME/docroot/modules/contrib/varbase_vmi_demo/scripts/
+## bash generate-varbase-testing-content.sh
+
+
+../../../../../vendor/drush/drush/drush pm:enable devel_generate
+
+../../../../../vendor/drush/drush/drush pm:enable varbase_media_default_content
+
+../../../../../vendor/drush/drush/drush devel-generate:terms tags 10;
+../../../../../vendor/drush/drush/drush devel-generate:terms blog_categories 5 ;
+
+../../../../../vendor/drush/drush/drush devel-generate:content --bundles=varbase_blog --languages='en' 50;
+../../../../../vendor/drush/drush/drush devel-generate:content --bundles=post --languages='en' 50;
+../../../../../vendor/drush/drush/drush devel-generate:content --bundles=episode --languages='en' 50;
+../../../../../vendor/drush/drush/drush devel-generate:content --bundles=faq --languages='en' 15;
+../../../../../vendor/drush/drush/drush devel-generate:content --bundles=varbase_heroslider_media --languages='en' 6;
+
